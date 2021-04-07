a1d31452c300e41dfa7c3911cea95c11e1a37184
Shahab-Athar <shahabathar1039@gmail.com>
2021-04-07 21:33:35 +0500

    Add Correct links

--- classlinks.py
+++ classlinks.py
@@ -1,9 +1,9 @@
 links = {
     "english": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng",
-    "Gsci": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng",
-    "Maths": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng",
-    "Comp": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng",
-    "Urdu": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng",
-    "Sst": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng",
-    "Isl": "https://us04web.zoom.us/j/8521795787?pwd=OtBop0pjkng"
+    "Gsci": "https://us04web.zoom.us/j/5034965413?pwd=Y2RsblgvQlNCcUpmMTVpRFE0bGY4dz09",
+    "Maths": "https://us04web.zoom.us/j/3381904420?pwd=cjJ5UVRSc0JEVVkzV01yYWZJM2JUUT09",
+    "Comp": "https://us04web.zoom.us/j/3802686793?pwd=U2xpeVMzVE80UERldk1PQUpaM3pNZz09",
+    "Urdu": "Pending",
+    "Sst": "https://us05web.zoom.us/j/8476447123?pwd=d2hhSElSQW5nZ0lnZTlobXBWeFdjQT09",
+    "Isl": "https://us04web.zoom.us/j/6652426806?pwd=WUFENzN3SmFaNlFSeTBLbWlXVklmUT09"
 }
